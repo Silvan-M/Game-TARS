f4c69480aaaccc51f82b57ddd43e8904b4e2ba50
Brian Funk <brian.funk@kswo.ch>
2020-07-30 20:20:04 +0200

    code commented until predict

--- dqn.py
+++ dqn.py
@@ -7,39 +7,43 @@ from statistics import mean
 from gym import wrappers
 
 
-class MyModel(tf.keras.Model):
+class MyModel(tf.keras.Model): # class with format tensorflow.keras.model, has ability to group layers into an object with training and inference (interpreter) features.
     def __init__(self, num_states, hidden_units, num_actions):
-        super(MyModel, self).__init__()
-        self.input_layer = tf.keras.layers.InputLayer(input_shape=(num_states,))
-        self.hidden_layers = []
-        for i in hidden_units:
-            self.hidden_layers.append(tf.keras.layers.Dense(
-                i, activation='tanh', kernel_initializer='RandomNormal'))
+        super(MyModel, self).__init__() # adds model as subclass to add the ability to differ the behaviour in training and interference
+        self.input_layer = tf.keras.layers.InputLayer(input_shape=(num_states,)) # define inputlayers, shape = determines dimension of vector [tuple]
+        self.hidden_layers = [] # clears the list of hidden layers
+        for i in hidden_units: 
+            self.hidden_layers.append(tf.keras.layers.Dense( 
+                i, activation='tanh', kernel_initializer='RandomNormal')) 
+                # activation = determines the output of a node with a specific input ['tanh','sigmoid']
+                # use_bias = uses bias vector [boolean] to add a value
+                # kernel_initializer = initialize random weights
         self.output_layer = tf.keras.layers.Dense(
             num_actions, activation='linear', kernel_initializer='RandomNormal')
 
-    @tf.function
+    @tf.function 
+    # constructs a callable that executes a tensorflow graph, which is a data structures that contain a set of tf.operation objects, which reoresent units of computation, can b restoredd even without the code
     def call(self, inputs):
-        z = self.input_layer(inputs)
-        for layer in self.hidden_layers:
-            z = layer(z)
-        output = self.output_layer(z)
-        return output
+        z = self.input_layer(inputs) # safe input layers with contained data as z
+        for layer in self.hidden_layers: 
+            z = layer(z) # iterates through all hidden layers with the previous output as input
+        output = self.output_layer(z) 
+        return output # returns processed data of last layer
 
 
 class DQN:
     def __init__(self, num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, lr):
-        self.num_actions = num_actions
-        self.batch_size = batch_size
-        self.optimizer = tf.optimizers.Adam(lr)
-        self.gamma = gamma
-        self.model = MyModel(num_states, hidden_units, num_actions)
-        self.experience = {'s': [], 'a': [], 'r': [], 's2': [], 'done': []}
-        self.max_experiences = max_experiences
-        self.min_experiences = min_experiences
+        self.num_actions = num_actions # 
+        self.batch_size = batch_size # amount of data processed at once
+        self.optimizer = tf.optimizers.Adam(lr) # adjusts the weight to minimize the loss function, ADAM uses momentum and bias correction
+        self.gamma = gamma #discount factor, weights importance of future reward [0,1]
+        self.model = MyModel(num_states, hidden_units, num_actions) # forwards to tf.keras.model
+        self.experience = {'s': [], 'a': [], 'r': [], 's2': [], 'done': []} # memorizes initial state, actions, rewards, achieved state
+        self.max_experiences = max_experiences # sets the maximum data stored as experience, if exceeded the oldest gets deleted
+        self.min_experiences = min_experiences # sets the start of the agent learning
 
     def predict(self, inputs):
-        return self.model(np.atleast_2d(inputs.astype('float32')))
+        return self.model(np.atleast_2d(inputs.astype('float32'))) # returns a prediction by a model, inputs get converted to float32 and forwarded to tf.keras.model
 
     def train(self, TargetNet):
         if len(self.experience['s']) < self.min_experiences:
