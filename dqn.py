c8d5c323f4252859f5efcd59cab2e9b5e755522d
Silvan-M <xbotraid@gmail.com>
2020-08-23 18:42:03 +0200

    Implemented two different NNs

--- dqn.py
+++ dqn.py
@@ -7,6 +7,16 @@ import random
 import log
 import games as g
 
+from tensorflow.python.framework import ops
+ops.reset_default_graph()
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten, InputLayer
+from keras.callbacks import TensorBoard
+from keras.optimizers import Adam
+from collections import deque
+import time
+
 class MyModel(tf.keras.Model): # class with format tensorflow.keras.model, has ability to group layers into an object with training and inference (interpreter) features.
     def __init__(self, num_states, hidden_units, num_actions):
         super(MyModel, self).__init__() # adds model as subclass to add the ability to differ the behaviour in training and interference
@@ -89,3 +99,256 @@ class DQN:
         variables2 = TrainNet.model.trainable_variables
         for v1, v2 in zip(variables1, variables2):
             v1.assign(v2.numpy())
+
+
+DISCOUNT = 0.999
+REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
+MIN_REPLAY_MEMORY_SIZE = 1_000  # Minimum number of steps in a memory to start training
+MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
+UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
+
+class DQNAgent:
+    def __init__(self):
+
+        # Main model
+        self.model = self.create_model()
+
+        # Target network
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+        # An array with last n steps for training
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+        self.epsilon = 0.9
+        self.MIN_EPSILON = 0.2
+        self.EPSILON_DECAY = 0.99
+        self.ep_rewards = []
+
+        self.ACTION_SPACE_SIZE = 9
+        # Custom tensorboard object
+
+        # Used to count when to update target network with main network's weights
+        self.target_update_counter = 0
+
+    def create_model(self):
+        model = Sequential()
+
+        model.add(InputLayer(9,))  # OBSERVATION_SPACE_VALUES = (10, 10, 3) a 10x10 RGB image.
+        model.add(Activation('relu'))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(27))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.2))
+
+        model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+        model.add(Dense(27))
+
+        model.add(Dense(9, activation='linear'))  # ACTION_SPACE_SIZE = how many choices (9)
+        model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
+        return model
+
+    # Adds step's data to a memory replay array
+    # (observation space, action, reward, new observation space, done)
+    def update_replay_memory(self, transition):
+        self.replay_memory.append(transition)
+
+    # Trains main network every step during episode
+    def train(self, terminal_state):
+
+        # Start training only if certain number of samples is already saved
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            
+            return
+
+
+        # Get a minibatch of random samples from memory replay table
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        # Get current states from minibatch, then query NN model for Q values
+        current_states = np.array([transition[0] for transition in minibatch])/255
+        current_qs_list = self.model.predict(current_states)
+
+        # Get future states from minibatch, then query NN model for Q values
+        # When using target network, query it, otherwise main network should be queried
+        new_current_states = np.array([transition[3] for transition in minibatch])/255
+        future_qs_list = self.target_model.predict(new_current_states)
+
+        X = []
+        y = []
+
+        # Now we need to enumerate our batches
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+            # If not a terminal state, get new q from future states, otherwise set it to 0
+            # almost like with Q Learning, but we use just part of equation here
+            print("Index: ",index,";Curren state: ", current_state,";Action: ",action,";Reward: ", reward, ";new_current_state: ",new_current_state, ";Done: ",done)
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+            # Update Q value for given state
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            # And append to our training data
+            X.append(current_state)
+            y.append(current_qs)
+
+        # Fit on all samples as one batch, log only on terminal state
+        self.model.fit(np.array(X)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False, callbacks=[] if terminal_state else None)
+
+        # Update target network counter every episode
+        if terminal_state:
+            self.target_update_counter += 1
+
+        # If counter reaches set value, update target network with weights of main network
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
+
+        self.append_reward_and_decay(reward)
+
+    # Queries main network for Q values given current observation space (environment state)
+    def get_qs(self, state):
+        return self.model.predict(np.array(state).reshape(-1, (9)))[0] # CHANGE SHAPE TO OBJECT ORIENTED
+        
+    def get_action(self, state):
+        if np.random.random() > self.epsilon:
+            # Get action from Q table
+            action = np.argmax(self.get_qs(state))
+        else:
+            # Get random action
+            action = np.random.randint(0, self.ACTION_SPACE_SIZE)
+        return action
+
+    def append_reward_and_decay(self, episode_reward):
+         # Append episode reward to a list and log stats (every given number of episodes)
+        self.ep_rewards.append(episode_reward)
+
+        # Decay epsilon
+        if self.epsilon > self.MIN_EPSILON:
+            self.epsilon *= self.EPSILON_DECAY
+            self.epsilon = max(self.MIN_EPSILON, self.epsilon)
+
+
+class DDDQN(tf.keras.Model):
+    def __init__(self):
+        super(DDDQN, self).__init__()
+        self.d1 = tf.keras.layers.Dense(128, activation='relu')
+        self.d2 = tf.keras.layers.Dense(128, activation='relu')
+        self.v = tf.keras.layers.Dense(1, activation=None)
+        self.a = tf.keras.layers.Dense(9, activation=None)
+
+    def call(self, input_data):
+        x = self.d1(input_data)
+        x = self.d2(x)
+        v = self.v(x)
+        a = self.a(x)
+        Q = v +(a -tf.math.reduce_mean(a, axis=1, keepdims=True))
+        return Q
+
+    def advantage(self, state):
+        x = self.d1(state)
+        x = self.d2(x)
+        a = self.a(x)
+        return a
+
+
+class exp_replay():
+    def __init__(self, buffer_size= 1000000):
+        self.buffer_size = buffer_size
+        self.state_mem = np.zeros((self.buffer_size, *(9,)), dtype=np.float32)
+        self.action_mem = np.zeros((self.buffer_size), dtype=np.int32)
+        self.reward_mem = np.zeros((self.buffer_size), dtype=np.float32)
+        self.next_state_mem = np.zeros((self.buffer_size, *(9,)), dtype=np.float32)
+        self.done_mem = np.zeros((self.buffer_size), dtype=np.bool)
+        self.pointer = 0
+
+    def add_exp(self, state, action, reward, next_state, done):
+        idx  = self.pointer % self.buffer_size 
+        self.state_mem[idx] = state
+        self.action_mem[idx] = action
+        self.reward_mem[idx] = reward
+        self.next_state_mem[idx] = next_state
+        self.done_mem[idx] = 1 - int(done)
+        self.pointer += 1
+
+    def sample_exp(self, batch_size= 64):
+        max_mem = min(self.pointer, self.buffer_size)
+        batch = np.random.choice(max_mem, batch_size, replace=False)
+        states = self.state_mem[batch]
+        actions = self.action_mem[batch]
+        rewards = self.reward_mem[batch]
+        next_states = self.next_state_mem[batch]
+        dones = self.done_mem[batch]
+        return states, actions, rewards, next_states, dones
+
+
+class agent():
+    def __init__(self, gamma=0.99, replace=100, lr=0.001):
+        self.gamma = gamma
+        self.epsilon = 1.0
+        self.min_epsilon = 0.01
+        self.epsilon_decay = 1e-3
+        self.replace = replace
+        self.trainstep = 0
+        self.memory = exp_replay()
+        self.batch_size = 64
+        self.model = DDDQN()
+        self.target_net = DDDQN()
+        opt = tf.keras.optimizers.Adam(learning_rate=lr)
+        self.model.compile(loss='mse', optimizer=opt)
+        self.target_net.compile(loss='mse', optimizer=opt)
+
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.choice([i for i in range(9)])
+
+        else:
+            actions = self.model.advantage(np.array([state]))
+            action = np.argmax(actions)
+            return action
+
+
+    
+    def update_mem(self, state, action, reward, next_state, done):
+        self.memory.add_exp(state, action, reward, next_state, done)
+
+
+    def update_target(self):
+        self.target_net.set_weights(self.model.get_weights())     
+
+    def update_epsilon(self):
+        self.epsilon = self.epsilon - self.epsilon_decay if self.epsilon > self.min_epsilon else self.min_epsilon
+        return self.epsilon
+
+        
+    def train(self):
+        if self.memory.pointer < self.batch_size:
+            return 
+        
+        if self.trainstep % self.replace == 0:
+            self.update_target()
+        states, actions, rewards, next_states, dones = self.memory.sample_exp(self.batch_size)
+        target = self.model.predict(states)
+        next_state_val = self.target_net.predict(next_states)
+        max_action = np.argmax(self.model.predict(next_states), axis=1)
+        batch_index = np.arange(self.batch_size, dtype=np.int32)
+        q_target = np.copy(target)
+        q_target[batch_index, actions] = rewards + self.gamma * next_state_val[batch_index, max_action]*dones
+        self.model.train_on_batch(states, q_target)
+        self.update_epsilon()
+        self.trainstep += 1
+
+    def save_model(self):
+        self.model.save("model.h5")
+        self.target_net.save("target_model.h5")
+
+
+    def load_model(self):
+        self.model = load_model("model.h5")
+        self.target_net = load_model("model.h5")
--- train_dqn.py
+++ train_dqn.py
@@ -8,119 +8,223 @@ import log
 import games as g
 import dqn as dqn
 
-def play_tictactoe(state, environment, TrainNet, TargetNet, epsilon, copy_step):
-    environment.reset()
-    rewards = 0
-    iter = 0
-    done = False
-    observations = state
-    losses = list()
-    illegal_moves = 0
-    while not done: # observes until game is done 
-        action = TrainNet.get_action(observations, epsilon) # TrainNet determines favorable action
-        prev_observations = observations # saves observations
-        # Uncomment following line if you want to test how a purely random agent performs
-        # action = random.randint(0,8)
-        result = environment.step_random(action)
-        observations = result[0]
-        reward = result[1]
-        done = result[2]
-        illegalmove = result[5]
-        if illegalmove:
-            illegal_moves += 1
-        if result[3] == 1:
-            won = True
-        else:
-            won = False
-        if result[4] == 1:
-            lose = True
-        else:
-            lose = False
-        rewards += reward        
-        exp = {'s': prev_observations, 'a': action, 'r': reward, 's2': observations, 'done': done} # make memory callable as a dictionary
-        TrainNet.add_experience(exp)# memorizes experience, if the max amount is exceeded the oldest element gets deleted
-        loss = TrainNet.train(TargetNet) # returns loss 
-        if isinstance(loss, int): # checks if loss is an integer
-            losses.append(loss)
-        else:
-            losses.append(loss.numpy()) # converted into an integer
-        iter += 1 # increment the counter
-        if iter % copy_step == 0: #copies the weights of the dqn to the TrainNet if the iter is a multiple of copy_step
-            TargetNet.copy_weights(TrainNet) 
-    return rewards, mean(losses), won, lose, illegal_moves #returns rewards and average
 
-def main(testing):
-    # Dict of all games for generalization purposes, values are:
-    # 0: play_game func, 1: Which environment to use, 2: Subfolder for checkpoints, log and figures, 3: Plotting func
-    games = {"tictactoe":[play_tictactoe,g.tictactoe,"tictactoe",log.plotTicTacToe]}
+class train_dqn():
+    def play_tictactoe(self, state, environment, epsilon, copy_step):
+        environment.reset()
+        rewards = 0
+        iter = 0
+        done = False
+        observations = state
+        losses = list()
+        illegal_moves = 0
+        while not done: # observes until game is done 
+            action = self.TrainNet.get_action(np.array(observations), epsilon) # TrainNet determines favorable action
+            print(np.array(observations))
+            print(np.array(observations).shape)
+            prev_observations = observations # saves observations
+            # Uncomment following line if you want to test how a purely random agent performs
+            # action = random.randint(0,8)
+            result = environment.step_random(action)
+            observations = result[0]
+            reward = result[1]
+            done = result[2]
+            illegalmove = result[5]
+            if illegalmove:
+                illegal_moves += 1
+            if result[3] == 1:
+                won = True
+            else:
+                won = False
+            if result[4] == 1:
+                lose = True
+            else:
+                lose = False
+            rewards += reward        
+            exp = {'s': np.array(prev_observations), 'a': action, 'r': reward, 's2': np.array(observations), 'done': done} # make memory callable as a dictionary
+            self.TrainNet.add_experience(exp)# memorizes experience, if the max amount is exceeded the oldest element gets deleted
+            loss = self.TrainNet.train(self.TargetNet) # returns loss 
+            if isinstance(loss, int): # checks if loss is an integer
+                losses.append(loss)
+            else:
+                losses.append(loss.numpy()) # converted into an integer
+            iter += 1 # increment the counter
+            if iter % copy_step == 0: #copies the weights of the dqn to the TrainNet if the iter is a multiple of copy_step
+                self.TargetNet.copy_weights(self.TrainNet) 
+        return rewards, mean(losses), won, lose, illegal_moves #returns rewards and average
+
+    def playNewModel(self, state, environment, epsilon, copy_step):
+        environment.reset()
+        rewards = 0
+        iter = 0
+        done = False
+        observations = state
+        losses = list()
+        illegal_moves = 0
+        while not done: # observes until game is done 
+            # This part stays mostly the same, the change is to query a model for Q values
+            
+            action = self.TrainNet.get_action(np.array(state))
+
+            prev_observations = observations # saves observations
+            # Uncomment following line if you want to test how a purely random agent performs
+            # action = random.randint(0,8)
+            result = environment.step_random(action)
+            observations = result[0]
+            state = observations
+            reward = result[1]
+            done = result[2]
+            illegalmove = result[5]
+            if illegalmove:
+                illegal_moves += 1
+            if result[3] == 1:
+                won = True
+            else:
+                won = False
+            if result[4] == 1:
+                lose = True
+            else:
+                lose = False
+            rewards += reward    
+            
+            exp = {'s': prev_observations, 'a': action, 'r': reward, 's2': observations, 'done': done} # make memory callable as a dictionary
+            # Every step we update replay memory and train main network
+            self.TrainNet.update_replay_memory((np.array(prev_observations), action, reward, np.array(observations), done))
+            self.TrainNet.train(np.array(observations))
+            # loss = TrainNet.train(TargetNet) # returns loss 
+            # if isinstance(loss, int): # checks if loss is an integer
+            #     losses.append(loss)
+            # else:
+            #     losses.append(loss.numpy()) # converted into an integer
+            # iter += 1 # increment the counter
+            # if iter % copy_step == 0: #copies the weights of the dqn to the TrainNet if the iter is a multiple of copy_step
+            #     TargetNet.copy_weights(TrainNet) 
+        return rewards, 50, won, lose, illegal_moves #returns rewards and average
     
-    # Here you can choose which of the games declared above you want to train, feel free to change!
-    game = games["tictactoe"]
+    def playModel2(self, state, environment, epsilon, copy_step):
+        environment.reset()
+        rewards = 0
+        iter = 0
+        done = False
+        observations = state
+        losses = list()
+        illegal_moves = 0
+        while not done:
+            action = self.TrainNet.act(state)
 
-    environment = game[1]()
-    state, gamma, copy_step, num_states, num_actions, hidden_units, max_experiences, min_experiences, batch_size, alpha, epsilon, min_epsilon, decay = environment.variables
-    # state: the initial state
-    # gamma: discount factor, weights importance of future reward [0,1]
-    # copy_step: the amount of episodes until the TargetNet gets updated
-    # num_states: Amount of states, num_actions: Amount of actions
-    # hidden_units: Amount of hidden neurons 
-    # max_experiences: sets the maximum data stored as experience, if exceeded the oldest gets deleted
-    # min_experiences: sets the start of the agent learning
-    # batch_size: amount of data processed at once
-    # alpha: learning rate, defines how drastically it changes weights
-    
-    TrainNet = dqn.DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, alpha)
-    TargetNet = dqn.DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, alpha)
+            prev_observations = observations # saves observations
+            # Uncomment following line if you want to test how a purely random agent performs
+            # action = random.randint(0,8)
+            result = environment.step_random(action)
+            observations = result[0]
+            state = observations
+            reward = result[1]
+            done = result[2]
+            illegalmove = result[5]
 
-    if testing:
-        model_name = "model.2020.08.12-19.42.21-I.100-N.1000"
-        directory = "tictactoe/models/"+model_name+"/TrainNet/"
-        TrainNet.model = tf.saved_model.load(directory)
-        directory = "tictactoe/models/"+model_name+"/TargetNet/"
-        TargetNet.model = tf.saved_model.load(directory)
-          
+            if illegalmove and False:
+                print(prev_observations)
+                print("Action: ",action, ";Done: ",done)
 
-    N = int(input("How many episodes do you want to train?\n"))
-    total_rewards = np.empty(N)
-    win_count = 0
-    lose_count = 0
-    log_interval = 100
+            
+            if illegalmove:
+                illegal_moves += 1
+            if result[3] == 1:
+                won = True
+            else:
+                won = False
+            if result[4] == 1:
+                lose = True
+            else:
+                lose = False
+            rewards += reward    
 
-    # For storing logs and model afterwards
-    current_time = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
-    timeAndInfo = current_time+"-I."+str(log_interval)+"-N."+str(N)
-    log_path = game[2]+"/logs/log."+timeAndInfo+".txt" # Model saved at "tictactoe/logs/log.Y.m.d-H:M:S-N.amountOfEpisodes.txt"
-    checkpoint_path = game[2]+"/models/model."+timeAndInfo # Model saved at "tictactoe/models/model.Y.m.d-H:M:S-N.amountOfEpisodes"
-    illegal_moves = 0
-    for n in range(N):
-        epsilon = max(min_epsilon, epsilon * decay)
-        total_reward, losses, won, lose, illegal_moves_game = game[0](state, environment, TrainNet, TargetNet, epsilon, copy_step)
-        if won:
-            win_count += 1
-        if lose:
-            lose_count += 1
-        total_rewards[n] = total_reward
-        avg_rewards = total_rewards[max(0, n - log_interval):(n + 1)].mean()
-        illegal_moves += illegal_moves_game
-        if (n % log_interval == 0) and (n != 0) or (n == N-1):
-            print("episode:", n, "episode reward:", total_reward, "eps:", epsilon, "avg reward (last "+str(log_interval)+"):", avg_rewards,
-                  "episode loss: ", losses, "wins: ",win_count, "lose: ", lose_count, "illegal moves: ",illegal_moves)
-            f = open(log_path, "a")
-            f.write((str(n)+";"+str(total_reward)+ ";"+str(epsilon)+";"+str(avg_rewards)+";"+ str(losses)+";"+ str(win_count))+";"+ str(lose_count)+";"+ str(illegal_moves)+"\n")
-            illegal_moves = 0
-            f.close()
-            win_count = 0
-            lose_count = 0
+            self.TrainNet.update_mem(prev_observations, action, reward, observations, done)
+            self.TrainNet.train()
 
-            # Save the models
-            tf.saved_model.save(TrainNet.model, checkpoint_path+"/TrainNet")
-            tf.saved_model.save(TargetNet.model, checkpoint_path+"/TargetNet")
-    print("avg reward for last 100 episodes:", avg_rewards)    
-    game[3](log_path)
+        return rewards, 50, won, lose, illegal_moves
+            
+    def main(self, testing):
+        # Dict of all games for generalization purposes, values are:
+        # 0: play_game func, 1: Which environment to use, 2: Subfolder for checkpoints, log and figures, 3: Plotting func
+        games = {"tictactoe":[self.playModel2,g.tictactoe,"tictactoe",log.plotTicTacToe]}
+        
+        # Here you can choose which of the games declared above you want to train, feel free to change!
+        game = games["tictactoe"]
+
+        environment = game[1]()
+        state, gamma, copy_step, num_states, num_actions, hidden_units, max_experiences, min_experiences, batch_size, alpha, epsilon, min_epsilon, decay = environment.variables
+        # state: the initial state
+        # gamma: discount factor, weights importance of future reward [0,1]
+        # copy_step: the amount of episodes until the TargetNet gets updated
+        # num_states: Amount of states, num_actions: Amount of actions
+        # hidden_units: Amount of hidden neurons 
+        # max_experiences: sets the maximum data stored as experience, if exceeded the oldest gets deleted
+        # min_experiences: sets the start of the agent learning
+        # batch_size: amount of data processed at once
+        # alpha: learning rate, defines how drastically it changes weights
+        
+        # self.TrainNet = dqn.DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, alpha)
+        # self.TargetNet = dqn.DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, alpha)
+
+        # self.TrainNet = dqn.DQNAgent()
+        # self.TargetNet = dqn.DQNAgent()
+
+        self.TargetNet = dqn.agent()
+        self.TrainNet = dqn.agent()
+
+        load = False
+        if load:
+            model_name = "model.2020.08.12-19.42.21-I.100-N.1000"
+            directory = "tictactoe/models/"+model_name+"/TrainNet/"
+            self.TrainNet.model = tf.saved_model.load(directory)
+            directory = "tictactoe/models/"+model_name+"/TargetNet/"
+            self.TargetNet.model = tf.saved_model.load(directory)
+            
+
+        N = int(input("How many episodes do you want to train?\n"))
+        total_rewards = np.empty(N)
+        win_count = 0
+        lose_count = 0
+        log_interval = 100
+
+        # For storing logs and model afterwards
+        current_time = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
+        timeAndInfo = current_time+"-I."+str(log_interval)+"-N."+str(N)
+        log_path = game[2]+"/logs/log."+timeAndInfo+".txt" # Model saved at "tictactoe/logs/log.Y.m.d-H:M:S-N.amountOfEpisodes.txt"
+        checkpoint_path = game[2]+"/models/model."+timeAndInfo # Model saved at "tictactoe/models/model.Y.m.d-H:M:S-N.amountOfEpisodes"
+        illegal_moves = 0
+        for n in range(N):
+            epsilon = max(min_epsilon, epsilon * decay)
+            total_reward, losses, won, lose, illegal_moves_game = game[0](state, environment, epsilon, copy_step)
+            if won:
+                win_count += 1
+            if lose:
+                lose_count += 1
+            total_rewards[n] = total_reward
+            #print(illegal_moves_game)
+            avg_rewards = total_rewards[max(0, n - log_interval):(n + 1)].mean()
+            illegal_moves += illegal_moves_game
+            if (n % log_interval == 0) and (n != 0) or (n == N-1):
+                print("episode:", n, "episode reward:", total_reward, "eps:", epsilon, "avg reward (last "+str(log_interval)+"):", avg_rewards,
+                    "episode loss: ", losses, "wins: ",win_count, "lose: ", lose_count, "illegal moves: ",illegal_moves)
+                f = open(log_path, "a")
+                f.write((str(n)+";"+str(total_reward)+ ";"+str(epsilon)+";"+str(avg_rewards)+";"+ str(losses)+";"+ str(win_count))+";"+ str(lose_count)+";"+ str(illegal_moves)+"\n")
+                illegal_moves = 0
+                f.close()
+                win_count = 0
+                lose_count = 0
+
+                # Save the models
+                tf.saved_model.save(self.TrainNet.model, checkpoint_path+"/TrainNet")
+                tf.saved_model.save(self.TargetNet.model, checkpoint_path+"/TargetNet")
+        print("avg reward for last 100 episodes:", avg_rewards)    
+        game[3](log_path)
 
 if __name__ == '__main__':
     # Set Parameter to true if you want to load the model on the path above and test it
-    main(False)
+    train_dqn = train_dqn()
+    train_dqn.main(False)
 
 
 
