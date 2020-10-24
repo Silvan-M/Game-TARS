40e5bd28dd9becd85f2b4fd3698d745ed1aaa457
Brian Funk <brian.funk@kswo.ch>
2020-10-24 13:22:16 +0200

    Some final changes (1)

--- Log_plotter.py
+++ Log_plotter.py
@@ -2,18 +2,20 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 # settings #####################################################
-file_name = 'test.txt' #input filename
-name = 'test' # name the plot
-label = ['n','total_reward','epsilon','avg_reward', 'losses', 'win_count', 'lose_count', 'illegal_moves'] # all saved data in csv file
-plot =[ 'win_count', 'lose_count'] # put in here what should be processed
+file_name = 'C:/Users/41763/Desktop/logs/SpaceInvadersFirstTrainingN500.txt' #input filename
+name = 'Snake - 5000 Ep - 2020.10.21' # name the plot
+
+#label = ['n','total_reward','epsilon','avg_reward', 'losses', 'win_count', 'lose_count', 'illegal_moves'] # all saved data in csv file
+label = ['n','total_reward','epsilon','avg_reward', 'losses', 'avg_points']
+plot =['n'] # put in here what should be processed
 color_mode = 'cyanred' #choose from gray, blue, red, yellow, cyanred, gremag, yelblue
-regression = True # make a regression 
-reg_dim = 14 # dimension of regression
-reg_mode = 'notnormal' #normal
+regression = False # make a regression 
+reg_dim = 1 # dimension of regression
+reg_mode = 'normal' #normal
 predict = False # if the prediction should be plottet
 range_predict = 10 # range of the prediction
 reg_func_inp =[1000000]
-save_file = False
+save_file = True
 ################################################################
 def color_brightener(color, dim =0): #brightens the color
     first = color[1:3]
@@ -41,7 +43,7 @@ def color_brightener(color, dim =0): #brightens the color
     return(hex_number)
 
 def color_generator(amount, scale): # generates equally distributed colors
-
+    wins = []
     colors = []
     difference = round((255/amount)-0.5)
     # hex numbers have 3 values  (red,gree,blue) each in hex in order to make colors just all numbers from 0 to 255 are valid
@@ -73,12 +75,12 @@ def color_generator(amount, scale): # generates equally distributed colors
     return(colors)
 
         
-
+wins = []
 row_data = []
 amount_of_rows = 0
 whole_data = []
 plot_data = []
-with open('test.txt') as csv_file:
+with open(file_name) as csv_file:
     # read csv file
     csv_reader = csv.reader(csv_file, delimiter=';')
     line_count = 0
@@ -88,13 +90,24 @@ with open('test.txt') as csv_file:
         for i in range(len(row)):
             row_data.append(row[i])
         whole_data.append(row_data)
-        row_data = []                   
+        wins.append(row_data[5])        
+        row_data = []           
     print(f'Processed {line_count} lines with {amount_of_rows} entries.')
 color = color_generator(len(plot)+1, 'cyanred')
 x=[]
+
+beg_av = (float(wins[0])+float(wins[2]))/2
+end_av = (float(wins[-1])+float(wins[-2]))/2
+print(f'Win in % first {beg_av} in the end {end_av}.')
 for i in range(line_count):
     x.append(int(whole_data[i][0]))
 plt.style.use('fivethirtyeight')
+params = {'legend.fontsize': 20,
+         'axes.labelsize':  9,
+         'axes.titlesize':  9,
+         'xtick.labelsize': 7,
+         'ytick.labelsize': 7}
+plt.rcParams.update(params)
 for q in range(len(plot)):
     i = label.index(plot[q])
     for y in range(line_count):
@@ -107,7 +120,7 @@ for q in range(len(plot)):
         poly1d_fn = np.poly1d(coef) 
 
         if dim == 1 and reg_mode == 'normal':
-            plt.plot(x, poly1d_fn(x), color = color_brightener(color[q]), label = "Avg. Incr. = "+str(round(coef[0]*len(x)*100,3))+"%",linewidth = 1.2, linestyle = (0,(5,5)))
+            plt.plot(x, poly1d_fn(x), color = color_brightener(color[q]), label = "Total Incr. = "+str(round(coef[0]*100*len(x),3))+"%",linewidth = 1.2, linestyle = (0,(5,5)))
         else:
             function = 'f(x) = '
             for i in range(len(coef)):
@@ -118,7 +131,7 @@ for q in range(len(plot)):
                     function = function + str(round(coef[i],4)) + ' x +'
                 else:
                     function = function + str(round(coef[i],4)) + ' x^'+str(h)+'+'
-            if dim > 4:
+            if dim > 12:
                 function =f'Regression function of {plot[q]}'
 
             plt.plot(x, poly1d_fn(x), color = color_brightener(color[q]), label = function,linewidth = 2, linestyle = (0,(5,5)))
@@ -137,11 +150,11 @@ for q in range(len(plot)):
                 
 
     plot_data=[]
-plt.title(str(name))
+plt.title(str(name), fontsize=12)
 plt.xlabel("Episodes")
 plt.ylabel("Value")
 plt.legend(loc="upper right",fontsize = 'x-small')
-if safe_file:
+if save_file:
     plt.savefig('fig.'+name+".pdf")
 plt.show()
 
